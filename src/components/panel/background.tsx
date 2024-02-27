336a3801e2ef8fbd3c7caa9b823bd4922f186e62
aquarazorda <g32.gelashvili@gmail.com>
2024-02-27 14:19:29 +0400

    feat: color picker & bg functionality

--- src/components/panel/background.tsx
+++ src/components/panel/background.tsx
@@ -1,15 +1,32 @@
 import { useHtml } from "@/state/html";
 import { Item } from "@/state/panel";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useShallow } from "zustand/react/shallow";
 import { Toggle } from "../ui/toggle";
+import { Popover, PopoverContent } from "../ui/popover";
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import { ColorResult, SketchPicker } from "react-color";
+import useDebouncedCallback from "@/lib/debounced-callback";
 
 export default function Background({
-  item: { selectors, title },
+  item: { selectors, title, defaultValue },
 }: {
   item: Item;
 }) {
-  const [styles] = useHtml(useShallow((state) => [state.styles]));
+  const [mode, setMode] = useState("color");
+  const [styles, swap] = useHtml(
+    useShallow((state) => [state.styles, state.swapStyles]),
+  );
+
+  const bgChange = useDebouncedCallback((color: ColorResult) => {
+    const newStyles = { ...styles };
+
+    selectors.forEach((selector) => {
+      newStyles[selector].background = color.hex;
+    });
+
+    swap(newStyles);
+  }, 200);
 
   const {
     isImage,
@@ -30,23 +47,49 @@ export default function Background({
     };
   }, [styles]);
 
+  useEffect(() => {
+    setMode(isImage ? "image" : "color");
+  }, [isImage]);
+
   return (
     <div className="rounded-2xl border border-[#DDD7E5] bg-white flex flex-col p-4 gap-4">
       <div className="flex justify-between items-center">
         <span className="text-base font-medium">{title}</span>
-        <Toggle defaultChecked={isImage}>Image</Toggle>
+        <Toggle
+          defaultChecked={isImage}
+          onPressedChange={(pressed) => setMode(pressed ? "image" : "color")}
+        >
+          Image
+        </Toggle>
       </div>
 
-      <div
-        className="rounded-[100px] py-4 px-5 flex justify-center"
-        onClick={() => {}}
-        style={{
-          height: "84.33px",
-          backgroundColor: !isImage ? bgColor : "#DDD7E5",
-        }}
-      >
-        {isImage && <img src={bgUrl} width={157} height={53} />}
-      </div>
+      {mode === "color" ? (
+        <Popover>
+          <PopoverTrigger>
+            <div
+              className="rounded-[100px] py-4 px-5 flex justify-center cursor-pointer"
+              style={{
+                height: "84.33px",
+                backgroundColor: bgColor ?? defaultValue,
+              }}
+            />
+          </PopoverTrigger>
+          <PopoverContent>
+            <SketchPicker onChangeComplete={bgChange} color={bgColor} />
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <div
+          className="rounded-[100px] py-4 px-5 flex justify-center cursor-pointer"
+          onClick={() => {}}
+          style={{
+            height: "84.33px",
+            backgroundColor: "#DDD7E5",
+          }}
+        >
+          <img src={bgUrl} width={157} height={53} />
+        </div>
+      )}
       <span className="text-[#877997] text-xs">
         * Image upload size and weight instrucitons
       </span>
