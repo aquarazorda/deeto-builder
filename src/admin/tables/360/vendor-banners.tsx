f6453f9b42b817216bfaa9844453c5aa330154af
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2024-03-18 19:55:20 +0400

    feat: image upload

--- src/admin/tables/360/vendor-banners.tsx
+++ src/admin/tables/360/vendor-banners.tsx
@@ -24,6 +24,28 @@ import {
 import useRemoveImageMutation from "@/queries/useRemoveImageMutation";
 import { useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import useUploadFile from "@/queries/useUploadFile";
+import useAddImage from "@/queries/useAddImage";
+import { useState } from "react";
 
 const columns: ColumnDef<CaseStudyImage>[] = [
   {
@@ -126,6 +148,146 @@ const columns: ColumnDef<CaseStudyImage>[] = [
   },
 ];
 
+const MAX_FILE_SIZE = 5000000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+const uploadNewSchema = z.object({
+  imageType: z.enum(["banner", "square"]),
+  image: z
+    .any()
+    .refine((files) => !!files, "Image is required.")
+    .refine((files) => files?.size <= MAX_FILE_SIZE, `Max file size is 5MB.`)
+    .refine(
+      (files) => ACCEPTED_IMAGE_TYPES.includes(files?.type),
+      ".jpg, .jpeg, .png and .webp files are accepted.",
+    ),
+});
+
+const UploadNew = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { vendorId } = useAdminState();
+  const queryClient = useQueryClient();
+
+  const { mutateAsync: uploadFile, isPending: isUploadPending } =
+    useUploadFile();
+  const { mutateAsync: addImage, isPending: isAddImagePending } = useAddImage();
+
+  const form = useForm<z.infer<typeof uploadNewSchema>>({
+    resolver: zodResolver(uploadNewSchema),
+    mode: "onChange",
+    defaultValues: {
+      imageType: "banner",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof uploadNewSchema>) => {
+    if (!vendorId) return;
+
+    try {
+      const imagePath = await uploadFile(values.image);
+      const res = await addImage({
+        imagePath,
+        imageType: values.imageType,
+        vendorId,
+        fileName: values.image.name,
+      });
+
+      if (res) {
+        queryClient.invalidateQueries({
+          queryKey: ["vendor-details", vendorId],
+          exact: true,
+        });
+
+        toast.success("Image uploaded successfully");
+        setIsOpen(false);
+      } else {
+        throw new Error("Failed to upload image");
+      }
+    } catch (_) {
+      toast.error("Failed to upload image");
+    }
+  };
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogTrigger asChild>
+        <Button>Upload new image</Button>
+      </AlertDialogTrigger>
+      <Form {...form}>
+        <AlertDialogContent className="min-w-96">
+          <AlertDialogHeader>
+            <AlertDialogTitle className="mb-4">
+              Upload new image
+            </AlertDialogTitle>
+            <AlertDialogDescription className="flex flex-col gap-4">
+              <FormField
+                control={form.control}
+                name="imageType"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Type</FormLabel>
+                    <Select
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <SelectTrigger>
+                        <FormControl>
+                          <SelectValue />
+                        </FormControl>
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="banner">Banner</SelectItem>
+                        <SelectItem value="square">Square</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field: { value, onChange, ...field } }) => (
+                  <FormItem>
+                    <FormLabel>Choose image</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="file"
+                        onChange={(event) => {
+                          onChange(event.target.files?.[0]);
+                        }}
+                        className="cursor-pointer"
+                        accept=".jpg, .jpeg, .png, .webp"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Close</AlertDialogCancel>
+            <AlertDialogAction
+              disabled={
+                !form.formState.isValid || isUploadPending || isAddImagePending
+              }
+              onClick={form.handleSubmit(onSubmit)}
+            >
+              Save
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </Form>
+    </AlertDialog>
+  );
+};
+
 export default function VendorBannersTable() {
   const { vendorId } = useAdminState();
   const { data, isLoading } = useGetVendorDetails(vendorId);
@@ -138,5 +300,12 @@ export default function VendorBannersTable() {
     return null;
   }
 
-  return <DataTable data={data.images} columns={columns} className="px-2" />;
+  return (
+    <DataTable
+      data={data.images}
+      columns={columns}
+      className="px-2"
+      renderSave={<UploadNew />}
+    />
+  );
 }
---
+++ src/queries/useUploadFile.ts
@@ -0,0 +1,38 @@
+import { useApi } from "@/state/api";
+import { useMutation } from "@tanstack/react-query";
+
+export default function useUploadFile() {
+  const { apiUrl } = useApi();
+
+  return useMutation({
+    mutationFn: async (file: File) => {
+      const { signedUrl } = await fetch(`${apiUrl}/v1/admin/newSignedUrl`)
+        .then((res) => res.json())
+        .then(
+          (res) =>
+            res as {
+              data: {
+                signedUploadUrl: {
+                  signedUrl: string;
+                  uniqueId: string;
+                };
+              };
+            },
+        )
+        .then(({ data }) => data.signedUploadUrl);
+
+      const body = new FormData();
+      body.append("file", file);
+
+      await fetch(signedUrl, {
+        method: "PUT",
+        headers: {
+          "Content-Type": file.type,
+        },
+        body,
+      });
+
+      return signedUrl;
+    },
+  });
+}
