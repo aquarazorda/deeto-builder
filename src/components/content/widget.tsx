35b43fefa9d8b844542e5971839454c3fbb355a4
aquarazorda <g32.gelashvili@gmail.com>
2024-06-03 15:05:41 +0400

    feat: popup mount target

--- src/components/content/widget.tsx
+++ src/components/content/widget.tsx
@@ -35,17 +35,35 @@ export default function WidgetContent({
           popupElement: HTMLElement;
           setExtra: (extra: Record<string, any>) => void;
         };
+
         customElements.whenDefined(`deeto-floating-reference`).then(() => {
-          const widgetStyle = document.createElement('style');
+          const widgetStyle = document.createElement("style");
           widgetStyle.innerHTML = `
             .cursor-pointer.fixed {
               bottom: 140px;
+              position: absolute;
             }
             .dt-floater-container {
               bottom: 120px;
             }`;
           element.shadowRoot?.appendChild(widgetStyle);
         });
+
+        customElements
+          .whenDefined("deeto-floating-reference-popup")
+          .then(() => {
+            const popupStyle = document.createElement("style");
+            popupStyle.innerHTML = `
+            .dt-embedded-reference-modal-index {
+            width: 100%;
+            position: absolute;
+            background-color: #00000038;
+            }
+            .dt-embedded-reference-modal-index > div:first-child {
+            display: none;
+            }`;
+            popupElement.shadowRoot?.appendChild(popupStyle);
+          });
 
         setUpdateExtra(() => setExtra);
 
