9fd91e880dc596e5719657dd9e91e9258bc1030a
aquarazorda <g32.gelashvili@gmail.com>
2024-06-07 16:13:50 +0400

    chore: config changes

--- src/config/config.prod.js
+++ src/config/config.prod.js
@@ -1,3 +1,2 @@
 export const ROOT_URL = "https://app.deeto.ai/html_builder";
-export const WIDGET_URL =
-  "https://deeto-widget.s3.amazonaws.com/assets/remoteEntry.js";
+export const WIDGET_URL = "https://deeto-widget.s3.amazonaws.com/assets/";
